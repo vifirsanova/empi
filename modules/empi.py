3d15fbfb8e24287048c01590efe33da1759737bf
Victoria <70071046+vifirsanova@users.noreply.github.com>
2024-04-27 16:37:23 +0300

    Update empi.py

--- modules/empi.py
+++ modules/empi.py
@@ -1,79 +1,84 @@
-class Search():
-  """
-  Поиск по графу для реализации двух сценариев
-  1. Шифрование данных: поиск данных, уязвимых для хакеров, с помощью графа знаний
-  - ищет указание на персональные данные пользователя с помощью поиска по базе знаний и шифрует их
-  - возвращает исходный текст с зашифрованными данными
-  2. Извлечение информации для генерации информативного ответа
-  - ищет по графу релевантную информацию и извлекает текст
-  - текст можно использовать для обусловленной генерации с помощью LLM
-  """
-  def __init__(self):
-    self.path = set()
+class Search:
+    """
+    Search class for graph-based search.
+    """
+    def __init__(self):
+        self.path = set()
 
-  def graph_search(self, data, query):
-    """
-    Рекурсивная функция для поиска совпадений (query) по графу (data):
-    - функция проходит по трем измерениям графа
-    - ищет точные совпадения или наличие поискового запроса внутри последовательности
-    """
-    for k in data:
-      if query in k.split() or query == k:
-        self.path.add(data[k]) if isinstance(data[k], dict) == False else self.path.add(str(data[k]))
-      if isinstance(data[k], dict):
-        self.graph_search(data=data[k], query=query)
-      else:
-        if isinstance(data[k], str):
-          if query in data[k].split() or query == data[k]:
-            self.path.add(data[k]) if isinstance(data[k], dict) == False else self.path.add(str(data[k]))
-        else:
-          for elem in data[k]:
-            if query in elem.split() or query == elem:
-              self.path.add(data[k]) if isinstance(data[k], dict) == False else self.path.add(str(data[k]))
-    return self.path
+    def graph_search(self, data, query):
+        """
+        Recursively search for matches (query) in the graph (data).
+
+        The function traverses three dimensions of the graph:
+        searches for exact matches or presence of the search query within the sequence.
+        
+        Args:
+          data (dict): The graph data to be searched.
+          query (str): The search query.
+
+        Returns: a set containing the path of matched nodes.
+        """
+        for k in data:
+            if query in k.split() or query == k:
+                self.path.add(data[k]) if not isinstance(data[k], dict) else self.path.add(str(data[k]))
+            if isinstance(data[k], dict):
+                self.graph_search(data=data[k], query=query)
+            else:
+                if isinstance(data[k], str):
+                    if query in data[k].split() or query == data[k]:
+                        self.path.add(data[k]) if not isinstance(data[k], dict) else self.path.add(str(data[k]))
+                else:
+                    for elem in data[k]:
+                        if query in elem.split() or query == elem:
+                            self.path.add(data[k]) if not isinstance(data[k], dict) else self.path.add(str(data[k]))
+        return self.path
 
 class Tokenizer:
     """
-    Класс "Токенизатор":
-    normalize(text) - нормализация кодировок по методу NFC, text (str)
-    clean(text, pattern) - чистка по заданному паттерну с помощью RegEx, text(str), pattern (паттерн вида r'...')
-    to_lower(text) - приведение к нижнему регистру, text (str)
-    segmentation(text, setting) - сегментация, text (str), setting ("symbol" для посимвольной, "word" для пословной, n (int) для n-граммной, "subword" для подсловной)
+    Tokenizer class for pre-processing.
+
+    The class provides methods for text normalizing text, cleaning text using regular expressions,
+    converting text to lowercase, segmenting text based on different settings, and tokenizing text.
     """
     def normalize(self, text):
         """
-        Нормализация текста по методу NFC.
+        Normalize the input text using the NFC (Normalization Form C) method.
         """
         return unicodedata.normalize('NFC', text)
 
     def clean(self, text, pattern):
         """
-        Очистка текста по заданному паттерну с помощью RegEx.
+        Clean the input text using a specified regular expression pattern.
         """
         return re.sub(pattern, '', text)
 
     def to_lower(self, text):
         """
-        Приведение текста к нижнему регистру.
+        Convert the input text to lowercase.
         """
         return text.lower()
 
     def segmentation(self, text, setting):
         """
-        Сегментация текста.
+        Segment the input text based on the specified setting.
+        Available settings:
+          setting='symbol' enables charater-based segmentation;
+          setting='word' enables word-based segmentation;
+          setting=n (int) enables n-gram segmentation;
+          setting='subword' enables byte-pair encoding (BPE) subword segmentation
         """
         text = re.sub('([.,!?()])', r' \1 ', text)
         text = re.sub('\s{2,}', ' ', text)
 
-        # посимвольная
+        # charater-based segmentation
         if setting == 'symbol':
             return list(text)
 
-        # пословная
+        # word-based segmentation
         elif setting == 'word':
             return text.split()
 
-        # n-граммная
+        # n-gram segmentation
         elif isinstance(setting, int):
             ngrams = []
             text = text.split()
@@ -81,15 +86,27 @@ class Tokenizer:
                 ngrams.append(' '.join(text[i:i + setting]))
             return ngrams
 
-        # подсловная
+        # byte-pair encoding (BPE) subword segmentation
         else:
             tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
             return tokenizer.tokenize(text)
           
-    def tokenize(self, text, patttern=None, setting):
+    def tokenize(self, text, setting, cleaning_pattern=None, lower=True):
+      """
+      Tokenize the input text.
+
+      Args:
+        text (str): input text to be tokenized
+        setting (str or int): segmentation setting; refer to segmentation method for details
+        cleaning_pattern (str, optional): regular expression pattern for cleaning; default is None
+        lower (Bool): enables (True) or disables (False) converting to lowercase; default is True 
+      
+      Returns: list of tokens.
+      """
       text = self.normalize(text)
-      if pattern is not None:
-        text = self.clean(text, pattern)
-      text = self.to_lower(text)
+      if cleaning_pattern is not None:
+        text = self.clean(text, cleaning_pattern)
+      if lower == True:
+        text = self.to_lower(text)
       tokens = self.segmentation(text, setting)
       return tokens
