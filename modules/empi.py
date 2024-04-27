ac063ffb58fda07df9ec3825d089ac062fa5976c
Victoria <70071046+vifirsanova@users.noreply.github.com>
2024-04-27 16:11:20 +0300

    Update empi.py

--- modules/empi.py
+++ modules/empi.py
@@ -32,47 +32,64 @@ class Search():
               self.path.add(data[k]) if isinstance(data[k], dict) == False else self.path.add(str(data[k]))
     return self.path
 
-class Tokenizer():
-  """
-  Класс "Токенизатор":
-  normalize(text) - нормализация кодировок по методу NFC, text (str)
-  clean(text, pattern) - чистка по заданному паттерну с помощью RegEx, text(str), pattern (паттерн вида r'...')
-  to_lower(text) - приведение к нижнему регистру, text (str)
-  segmentation(text, setting) - сегментация, text (str), setting ("symbol" для посимвольной, "word" для пословной, n (int) для n-граммной, "subword" для подсловной)
-  """
-  def __init__(self):
-    pass
+class Tokenizer:
+    """
+    Класс "Токенизатор":
+    normalize(text) - нормализация кодировок по методу NFC, text (str)
+    clean(text, pattern) - чистка по заданному паттерну с помощью RegEx, text(str), pattern (паттерн вида r'...')
+    to_lower(text) - приведение к нижнему регистру, text (str)
+    segmentation(text, setting) - сегментация, text (str), setting ("symbol" для посимвольной, "word" для пословной, n (int) для n-граммной, "subword" для подсловной)
+    """
+    def normalize(self, text):
+        """
+        Нормализация текста по методу NFC.
+        """
+        return unicodedata.normalize('NFC', text)
 
-  def normalize(self, text):
-    return unicodedata.normalize('NFC', text)
+    def clean(self, text, pattern):
+        """
+        Очистка текста по заданному паттерну с помощью RegEx.
+        """
+        return re.sub(pattern, '', text)
 
-  def clean(self, text, pattern):
-    return re.sub(pattern, '', text)
+    def to_lower(self, text):
+        """
+        Приведение текста к нижнему регистру.
+        """
+        return text.lower()
 
-  def to_lower(self, text):
-    return text.lower()
+    def segmentation(self, text, setting):
+        """
+        Сегментация текста.
+        """
+        text = re.sub('([.,!?()])', r' \1 ', text)
+        text = re.sub('\s{2,}', ' ', text)
 
-  def segmentation(self, text, setting):
-    text = re.sub('([.,!?()])', r' \1 ', text)
-    text = re.sub('\s{2,}', ' ', text)
+        # посимвольная
+        if setting == 'symbol':
+            return list(text)
 
-    # посимвольная
-    if setting == 'symbol':
-      return [*text]
+        # пословная
+        elif setting == 'word':
+            return text.split()
 
-    # пословная
-    elif setting == 'word':
-      return text.split()
+        # n-граммная
+        elif isinstance(setting, int):
+            ngrams = []
+            text = text.split()
+            for i in range(len(text) - setting + 1):
+                ngrams.append(' '.join(text[i:i + setting]))
+            return ngrams
 
-    # n-граммная
-    elif isinstance(setting, int):
-      ngrams = []
-      text = text.split()
-      for i in range(len(text) - setting+1):
-        ngrams.append(' '.join(text[i:i + setting]))
-      return ngrams
+        # подсловная
+        else:
+            tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
+            return tokenizer.tokenize(text)
 
-    # подсловная
-    else:
-      tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
-      return tokenizer.tokenize(text)
+  def tokenize(self, text, patttern=None, setting):
+    text = self.normalize(text)
+    if pattern is not None:
+      text = self.clean(text, pattern)
+    text = self.to_lower(text)
+    tokens = self.segmentation(text, setting)
+    return tokens
