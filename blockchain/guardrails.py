3f0e265407c7d5bfa36522fb226eaae6a2fc3677
vifirsanova <vifirsanova@gmail.com>
2024-09-16 17:06:03 +0300

    update guardrails.py

--- blockchain/guardrails.py
+++ blockchain/guardrails.py
@@ -4,6 +4,8 @@ from langchain_community.llms.llamacpp import LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+from llama_cpp import Llama
+
 def process_rule(rule_name, rule_data):
   """
   Process a single rule entry from the data, handling special cases for 'connotation' and 'implicature'.
@@ -40,6 +42,27 @@ for rule_name, rule_data in data['rules'].items():
   system_rule += 'ОБРАЗЦЫ ЗАМЕН\n'
   system_rule += f'{substitutions}\n\n'
   i += 1
+
+llm = Llama(
+      model_path="/home/missvector/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf",
+      n_ctx=2048, # context window
+)
+
+prompt_template = f"""{system_rule}
+Дан образец <Всего лишь 10% участников.>
+Определить, какое правило нарушено в текущем образце. Произвести необходимую замену согласно правилу.
+"""
+
+print(prompt_template)
+
+output = llm(
+  f"Q: {prompt_template} A: ",  # Prompt
+  max_tokens=64,  # Generate up to 64 tokens, set to None to generate up to the end of the context window
+  stop=["Q:", "\n"],  # Stop generating just before the model would generate a new question
+  # echo=True  # Echo the prompt back in the output
+)
+
+print(output['choices'][0]['text'])
 
 def build_chain(system_rule):
    """
